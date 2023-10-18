17abddbe0a61881e8648b3dd366dde49a61b0dc1
ChocoScaff <anjul2@hotmail.fr>
2023-10-18 09:22:17 +0200

    add player 2

--- console.py
+++ console.py
@@ -5,6 +5,8 @@ Created on Thu Oct 12 09:26:36 2023
 @author: PC
 """
 
+
+
 def console_afficheGrille (grille) : 
     """Affiche le rectangle d'etoiles  (tout ceci en meme
     temps et non pas le rectangle puis la )  """
